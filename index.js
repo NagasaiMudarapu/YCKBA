25bf21441ae7fca95cc5f293c1639ec6b14019b3
NagasaiMudarapu <66234414+NagasaiMudarapu@users.noreply.github.com>
2023-08-31 23:51:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app=express();
+
+var PORT = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/FrontPage.html');
+});
+app.get('/resume',function(req,res){
+    res.sendFile(__dirname+'/Resume.html');
+});
+app.get('/signin',function(req,res){
+    res.sendFile(__dirname+'/SignIn.html');
+});
+app.get('/signup',function(req,res){
+    res.sendFile(__dirname+'/SignUp.html');
+});
+app.get('/contact',function(req,res){
+    res.sendFile(__dirname+'/Contact.html');
+});
+
+app.listen(PORT, function(){
+    console.log("http://localhost:"+PORT);
+});
+
+//ctrl+c for terminating server.
